519ff7c749c8fa5ca0ebeb195160211986513f1a
PauloBarros-creator <62844951+PauloBarros-creator@users.noreply.github.com>
2021-05-14 11:34:51 +0100

    .

--- Vamos_Brincar/Controllers/AtiController.cs
+++ Vamos_Brincar/Controllers/AtiController.cs
@@ -59,6 +59,27 @@ namespace Vamos_Brincar.Controllers
         }
 
         // GET: Ati/Edit/5
+        public ActionResult EditInst(int id)
+        {
+
+            return View(ci.GetAti().Find(itermodel
+                => itermodel.id_atividade == id));
+
+        }
+        [HttpPost]
+        public ActionResult EditInst(int id, CrudProp updateati)
+        {
+            try
+            {
+                // TODO: Add update logic here
+                ci.editAti(updateati);
+                return RedirectToAction("ConAtiIns");
+            }
+            catch
+            {
+                return View();
+            }
+        }
         public ActionResult Edit(int id)
         {
     
