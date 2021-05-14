2c5514c29373f3e0e0e8a3850cd7df3b9717d517
PauloBarros-creator <62844951+PauloBarros-creator@users.noreply.github.com>
2021-05-14 11:10:24 +0100

    .

--- Vamos_Brincar/Controllers/AtiController.cs
+++ Vamos_Brincar/Controllers/AtiController.cs
@@ -74,7 +74,7 @@ namespace Vamos_Brincar.Controllers
             {
                 // TODO: Add update logic here
                 ci.editAti(updateati);
-                return RedirectToAction("Index");
+                return RedirectToAction("ConAtiIns");
             }
             catch
             {
